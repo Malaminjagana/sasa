16e18a36b2a275149fb0b71e5d697f99b30a8fca
malamin jagana <jaganalamin@gmail.com>
2026-07-16 10:32:27 +0200

    replace the old

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
-
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
-const isGitHubPages =
-  process.env.NODE_ENV === 'production' && process.env.GITHUB_PAGES
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [react()],
-  base: isGitHubPages ? '/sasa/' : '/',
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/sasa/',
 })
+
