fb4c5a233ea8775ccb9b4a714fbeb938973ff6ee
malamin jagana <jaganalamin@gmail.com>
2026-07-16 10:21:34 +0200

    vercel is added

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
+const isGitHubPages =
+  process.env.NODE_ENV === 'production' && process.env.GITHUB_PAGES
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  base: '/sasa/',
+  plugins: [react()],
+  base: isGitHubPages ? '/sasa/' : '/',
 })
-
